1e0508658e7c5b4478c0b7587ae1f7866c0fbd47
Rohit Prashanth Kannam <rk21155@corp.ojas-it.com>
2021-07-22 13:13:39 +0530

    modified bio page

--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\rk21155\pycharmprojects\portfolio\venv\scripts\python.exe
+#!c:\users\rk21155\desktop\rohit's portfolio\portfolio\venv\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
